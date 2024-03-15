b44e544f09e5676e48b4e9e5d351bd2ea31f3c76
Baiketli Dijo <baiketlid@gmail.com>
2024-03-15 10:45:34 +0200

    Add project files.

---
+++ BackgroundDemoApp/SampleData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Concurrent;
+
+namespace BackgroundDemoApp
+{
+    public class SampleData
+    {
+        // Thread safe list.
+        public ConcurrentBag<string> Data { get; set; } = new();
+    }
+}
